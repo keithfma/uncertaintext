d52c2bf1d501d1e09e6d4d5a83a458ae6d9a39b1
Keith Ma <keithfma@gmail.com>
2022-03-31 23:05:43 -0400

    Add uncertaintext.py with a minimal example printing text at the command line

---
+++ uncertaintext.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""Uncertaintext python prototype
+
+Exploring the basic idea of representing uncertain values with samples from the
+underlying distribution
+"""
+
+import click
+from random import gauss
+import time
+
+
+@click.command()
+@click.option('--mu', type=float, default=0., help='Mean, parameter to gaussian distribution')
+@click.option('--sigma', type=float, default=1., help='Standard deviation, parameter to gaussian distribution')
+@click.option('--fps', type=int, default=10, help='Target update rate, in "frames per second"')
+@click.option('--duration', type=float, default=5, help='Demo duration in seconds')
+def cli(mu: float, sigma: float, fps: int, duration: float):
+    """Display uncertaintext representation of the specified distribution
+    """
+    delay = 1. / fps
+    exit_time  = time.monotonic() + duration
+
+    while (now := time.monotonic()) < exit_time:
+        click.echo(f'{gauss(mu, sigma): .2f}\r', nl=False)
+        time.sleep(max(0, (now + delay) - time.monotonic()))
+        
+
+if __name__ == '__main__':
+    cli()
