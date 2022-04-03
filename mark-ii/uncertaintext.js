9e289491a028aca526e96264150e025e3eb7019b
Keith Ma <keithfma@gmail.com>
2022-04-03 14:45:07 -0400

    Mark II, now with better js code

---
+++ mark-ii/uncertaintext.js
@@ -0,0 +1,71 @@
+// See: https://github.com/d3/d3-format
+// FIXME: will want a hard-copy of this, assuming the license is OK
+
+// Standard normal variate using Box-Muller transform.
+// see: https://stackoverflow.com/a/36481059
+function randn_bm() {
+   var u = 0;
+   var v = 0;
+   while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+   while (v === 0) v = Math.random();
+   return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+}
+
+// retrieve dataset attribute or fail if not set
+function required_data(element, name) {
+    return element.dataset[name]
+}
+
+// retrieve dataset attribute or default if not set
+function optional_data(element, name, fallback) {
+    if (name in element.dataset) {
+        return element.dataset[name]
+    }
+    return fallback
+}
+
+
+// update element with formatted sample from the distribution
+function uncertaintext(target, mu, sigma, fmt_mu, fmt_sigma, fmt_sample) {
+    var sample = randn_bm()*sigma + mu;
+    target.innerHTML = `${fmt_mu(mu)} &plusmn; ${fmt_sigma(sigma)} (${fmt_sample(sample)})`
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log('Uncertaintext Mark II Activated')
+
+    
+    targets = document.getElementsByClassName("uncertaintext")
+
+    for (let i = 0; i < targets.length; i++) {
+
+        let target = targets[i];
+
+        // distribution definition (required)
+        let mu    = parseFloat(required_data(target, 'mu'));
+        let sigma = parseFloat(required_data(target, 'sigma'));
+
+        // format specifications (optional) 
+        let fmt_mu     = optional_data(target, 'fmtMu', " .2f");
+        let fmt_sigma  = optional_data(target, 'fmtSigma', " .2f");
+        let fmt_sample = optional_data(target, 'fmtSample', fmt_sigma);
+
+        // update interval (optional) 
+        let fps = optional_data(target, 'fps', 5);
+        let delay_ms = 1. / fps * 1000.
+
+        // start updating 
+        setInterval(
+            uncertaintext,
+            delay_ms,
+            target,
+            mu,
+            sigma,
+            d3.format(fmt_mu),
+            d3.format(fmt_sigma),
+            d3.format(fmt_sample)
+        );        
+    } 
+});
