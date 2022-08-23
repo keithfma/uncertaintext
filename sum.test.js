1aec8d3cc4938aba6333d4064fe590c66ee9b1cf
Keith Ma <keithfma@gmail.com>
2022-08-23 11:12:59 -0400

    Enable DOM access in tests via "jsdom"

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,9 @@ import {sum} from './sum.js';
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
