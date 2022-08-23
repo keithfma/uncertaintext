feab5b667d00c9e6d4ef27e5d090a2604b5787fe
Keith Ma <keithfma@gmail.com>
2022-08-23 16:09:08 -0400

    Stashed before abandoning

--- src/uncertaintext.test.js
+++ src/uncertaintext.test.js
@@ -1,22 +1,28 @@
-import {jest} from '@jest/globals';
+import test from 'ava';
+import {JSDOM } from 'jsdom';
 import {get_required_data, get_optional_data, get_sampler} from './uncertaintext.js';
-import {randomNormal} from "d3-random";
-//import * as D3 from "d3-random";
-
-jest.mock(randomNormal);
 
 
-test('get_required_data happy path', () => {
+function new_doc() {
+  const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+  return dom.window.document;
+}
 
-  // create document with an expected dataset attribute
+
+test('get_required_data happy path', t => {
+
+  let document = new_doc();
+
   document.body.innerHTML = '<div id=test-element data-uct-distrib=normal></div>';
   let element = document.getElementById('test-element');
 
-  // retrieve the dataset value by its mangled name
-  expect(get_required_data(element, 'uctDistrib')).toBe('normal');
+  let result = get_required_data(element, 'uctDistrib')
+
+  t.is(result, 'normal');
 });
 
 
+/*
 test('get_required_data fails if attribute is not defined', () => {
 
   // create document with a dataset attribute
@@ -48,18 +54,11 @@ test('get_optional_data returns default if attribute is not defined', () => {
   // fail to retrieve undefined dataset element
   expect(get_optional_data(element, 'anUndefinedAttribute', 'default')).toBe('default');
 });
+*/
 
 
-test('get_sampler normal distribution happy path', () => {
-
-  // setup mock
-  //console.log(D3);
-  spy = jest.spyOn(D3, 'randomNormal');
-
-  let sampler = get_sampler(element);
-  console.log(sampler)
-  console.log(JSON.stringify(sampler, null, 4))
-
+test('get_sampler normal distribution happy path', t => {
+    t.pass()
 });
 
 
