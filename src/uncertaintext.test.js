448de51c56aae476a71aecfd1c694f69d6034f9e
Keith Ma <keithfma@gmail.com>
2022-08-23 16:09:40 -0400

    Revert "Stashed before abandoning"
    This reverts commit feab5b667d00c9e6d4ef27e5d090a2604b5787fe.

--- src/uncertaintext.test.js
+++ src/uncertaintext.test.js
@@ -1,28 +1,22 @@
-import test from 'ava';
-import {JSDOM } from 'jsdom';
+import {jest} from '@jest/globals';
 import {get_required_data, get_optional_data, get_sampler} from './uncertaintext.js';
+import {randomNormal} from "d3-random";
+//import * as D3 from "d3-random";
+
+jest.mock(randomNormal);
 
 
-function new_doc() {
-  const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
-  return dom.window.document;
-}
+test('get_required_data happy path', () => {
 
-
-test('get_required_data happy path', t => {
-
-  let document = new_doc();
-
+  // create document with an expected dataset attribute
   document.body.innerHTML = '<div id=test-element data-uct-distrib=normal></div>';
   let element = document.getElementById('test-element');
 
-  let result = get_required_data(element, 'uctDistrib')
-
-  t.is(result, 'normal');
+  // retrieve the dataset value by its mangled name
+  expect(get_required_data(element, 'uctDistrib')).toBe('normal');
 });
 
 
-/*
 test('get_required_data fails if attribute is not defined', () => {
 
   // create document with a dataset attribute
@@ -54,11 +48,18 @@ test('get_optional_data returns default if attribute is not defined', () => {
   // fail to retrieve undefined dataset element
   expect(get_optional_data(element, 'anUndefinedAttribute', 'default')).toBe('default');
 });
-*/
 
 
-test('get_sampler normal distribution happy path', t => {
-    t.pass()
+test('get_sampler normal distribution happy path', () => {
+
+  // setup mock
+  //console.log(D3);
+  spy = jest.spyOn(D3, 'randomNormal');
+
+  let sampler = get_sampler(element);
+  console.log(sampler)
+  console.log(JSON.stringify(sampler, null, 4))
+
 });
 
 
