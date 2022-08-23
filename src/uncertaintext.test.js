d3025083faccb6b03f72da3d54bdce285a39c478
Keith Ma <keithfma@gmail.com>
2022-08-23 16:13:05 -0400

    Give up on mocking, which should be easy ffs.

--- src/uncertaintext.test.js
+++ src/uncertaintext.test.js
@@ -1,65 +1,54 @@
 import {jest} from '@jest/globals';
 import {get_required_data, get_optional_data, get_sampler} from './uncertaintext.js';
-import {randomNormal} from "d3-random";
-//import * as D3 from "d3-random";
-
-jest.mock(randomNormal);
 
 
 test('get_required_data happy path', () => {
 
-  // create document with an expected dataset attribute
   document.body.innerHTML = '<div id=test-element data-uct-distrib=normal></div>';
   let element = document.getElementById('test-element');
+  
+  let result = get_required_data(element, 'uctDistrib') 
 
-  // retrieve the dataset value by its mangled name
-  expect(get_required_data(element, 'uctDistrib')).toBe('normal');
+  expect(result).toBe('normal');
 });
 
 
 test('get_required_data fails if attribute is not defined', () => {
 
-  // create document with a dataset attribute
   document.body.innerHTML = '<div id=test-element data-uct-distrib=normal></div>';
   let element = document.getElementById('test-element');
 
-  // fail to retrieve undefined dataset element
   expect(() => get_required_data(element, 'anUndefinedAttribute')).toThrow()
 });
 
 
 test('get_optional_data happy path', () => {
 
-  // create document with an expected dataset attribute
   document.body.innerHTML = '<div id=test-element data-uct-distrib=normal></div>';
   let element = document.getElementById('test-element');
 
-  // retrieve the dataset value by its mangled name
-  expect(get_optional_data(element, 'uctDistrib', 'default-value')).toBe('normal');
+  let result = get_optional_data(element, 'uctDistrib', 'default-value')
+
+  expect(result).toBe('normal');
 });
 
 
 test('get_optional_data returns default if attribute is not defined', () => {
 
-  // create document with a dataset attribute
   document.body.innerHTML = '<div id=test-element data-uct-distrib=normal></div>';
   let element = document.getElementById('test-element');
 
-  // fail to retrieve undefined dataset element
   expect(get_optional_data(element, 'anUndefinedAttribute', 'default')).toBe('default');
 });
 
 
 test('get_sampler normal distribution happy path', () => {
 
-  // setup mock
-  //console.log(D3);
-  spy = jest.spyOn(D3, 'randomNormal');
+  document.body.innerHTML = '<div id=test-element data-uct-distrib=normal data-uct-mu=1 data-uct-sigma=2></div>';
+  let element = document.getElementById('test-element');
 
   let sampler = get_sampler(element);
-  console.log(sampler)
-  console.log(JSON.stringify(sampler, null, 4))
-
+  console.log(sampler())
 });
 
 
