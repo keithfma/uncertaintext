8b36425f3b7d39a6e86f48953602057674374dac
Keith Ma <keithfma@users.noreply.github.com>
2023-06-04 14:56:57 -0400

    Improve documentation (#3)

--- src/uncertaintext.js
+++ src/uncertaintext.js
@@ -1,24 +1,6 @@
 /** 
 * Uncertaintext: uncertain text for uncertain numbers
 *
-* Uncertaintext provides dynamic styling for representing probability distributions. The core idea is
-* to express probabalistic values as series of random samples from the distribution, rather than the
-* usual summary statistics. The aim is to give readers a "gut feel" for the uncertainty, and have
-* a little fun doing it.
-*
-* To use, include this script in your webpage as a module, and then define
-* your distributions as <span> elements with the following attributes:
-*  * class=uncertaintext: required, marks the span element for uncertaintext to find and update
-*  * data-uct-distrib=[string], required, name of the distribution to sample from, currently supported
-*      names are: uniform, normal.
-*  * data-uct-min=[float]: required for uniform distribution, minimum value
-*  * data-uct-max=[float]: required for uniform distribution, maximum value
-*  * data-uct-mu=[float]: required for normal distribution, mean
-*  * data-uct-sigma=[float]: required for normal distribution, standard deviation
-*  * data-uct-format=[string]: optional, printf-style format string to apply to the sample,
-*      see https:* github.com/d3/d3-format
-*  * data-uct-fps=[int]: optional, update frequency in "frames" per second
-*
 * Dependencies:
 *  * d3-format: https:* github.com/d3/d3-format
 *  * d3-random: https:* github.com/d3/d3-random
